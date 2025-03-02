8972c6d4af1117e1d5a209580e7add521281029c
Alexis Gros <alexis.gros99@gmail.com>
2025-03-02 22:50:55 +1300

    refactor drums.py

---
+++ drums.py
@@ -0,0 +1,48 @@
+import math
+from typing import List
+
+
+class DrumLayer:
+    distance_from_core: float
+    radius: float
+
+
+class CoreDesc:
+    core_diameter: float
+    core_height: float
+    shielding_thickness: float
+
+
+def calculate_drum_arc_length(drum: DrumLayer, core_radius: float):
+    return (
+        2
+        * drum.radius
+        * math.acos(
+            (drum.distance_from_core**2 + drum.radius**2 - core_radius**2)
+            / (2 * drum.distance_from_core * drum.radius)
+        )
+    )
+
+
+def calculate_drum_surface_in_core(drum: DrumLayer, core: CoreDesc):
+    return calculate_drum_arc_length(drum, core.core_radius) * core.core_height
+
+
+def calculate_drum_layers_radiuses(
+    outer_drum: DrumLayer, core_radius: float, distance_between_drums: float
+) -> List[float]:
+    drum_radiuses = [outer_drum.distance_from_core]
+    while (
+        drum_radiuses[-1] - (outer_drum.radius - core_radius) > distance_between_drums
+    ):
+        drum_radiuses.append(drum_radiuses[-1] - distance_between_drums)
+    return drum_radiuses
+
+
+def calculate_drum_layers_surface_in_core(
+    drums: List[DrumLayer], core: CoreDesc
+) -> float:
+    drum_surface_in_core = 0
+    for drum in drums:
+        drum_surface_in_core += calculate_drum_surface_in_core(drum, core)
+    return drum_surface_in_core
