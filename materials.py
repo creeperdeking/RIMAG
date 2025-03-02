2e643bae441f3065bf487b46848ca2c3c4eaf2fe
Alexis Gros <alexis.gros99@gmail.com>
2025-03-02 22:03:53 +1300

    add materials

--- materials.py
+++ materials.py
@@ -1,13 +1,21 @@
-from typing import List, Optional
+from typing import List, Optional, Dict
 
 
 class Atom:
-    name: str
+    atomic_weight: float
+
+
+class AtomProportion:
+    atom: Atom
+    proportion: Optional[float] = 1
+
+    def __init__(self, atom: Atom, proportion: Optional[float] = 1):
+        self.atom = atom
+        self.proportion = proportion
 
 
 class Material:
-    name: str
-    composition: List[Atom]
+    composition: List[AtomProportion]
     density: float  # g/cm3
     specific_heat: Optional[float] = None  # J/g/K
     thermal_conductivity: Optional[float] = None  # W/cm/K
@@ -15,14 +23,56 @@ class Material:
     boiling_point: Optional[float] = None  # K
 
 
-materials: List[Material] = [
-    {
-        "name": "Uranium",
-        "composition": [Atom(name="U-235")],
-        "density": 18.95,
-        "specific_heat": 0.032,
-        "thermal_conductivity": 0.027,
-        "melting_point": 1408,
-        "boiling_point": 4131,
-    }
-]
+atoms: Dict[str, Atom] = {
+    "U235": Atom(atomic_weight=235.0439299),
+    "U238": Atom(atomic_weight=238.0507884),
+    "H": Atom(atomic_weight=1.00794),
+    "O": Atom(atomic_weight=15.9994),
+    "D": Atom(atomic_weight=2.01410177812),
+    "W": Atom(atomic_weight=183.84),
+    "C": Atom(atomic_weight=12.0107),
+}
+
+uranium = Material(
+    composition=[
+        AtomProportion(atoms["U235"], 0.00711),
+        AtomProportion(atoms["U238"], 0.99289),
+    ],
+    density=18.95,
+)
+
+materials: Dict[str, Material] = {
+    "Uranium": uranium,
+    "Water": Material(
+        composition=[
+            AtomProportion(atoms["H"], 2),
+            AtomProportion(atoms["O"], 1),
+        ],
+        density=1,
+    ),
+    "Heavy Water": Material(
+        composition=[
+            AtomProportion(atoms["D"], 2),
+            AtomProportion(atoms["O"], 1),
+        ],
+        density=1.105,
+    ),
+    "Tungsten": Material(
+        composition=[
+            AtomProportion(atoms["W"]),
+        ],
+        density=19.25,
+    ),
+    "Uranium Dioxide": Material(
+        composition=uranium.composition + [AtomProportion(atoms["O"], 2)],
+        density=10.97,
+    ),
+    "Uranium Carbide": Material(
+        composition=uranium.composition + [AtomProportion(atoms["C"], 1)],
+        density=11.69,
+    ),
+    "Graphite": Material(
+        composition=[AtomProportion(atoms["C"])],
+        density=1.8,
+    ),
+}
